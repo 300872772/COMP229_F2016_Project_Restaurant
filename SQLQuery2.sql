e81eb7a91f64eb049540126b3d89b218b2baae33
Aangi <aashah.6198@gmail.com>
2016-11-25 23:34:38 -0500

    query update

---
+++ SQLQuery2.sql
@@ -0,0 +1,46 @@
+﻿create table ManueTime (
+MenuTimeID SMALLINT primary key,
+MenueTimeName VARCHAR(20));
+
+Drop Table ManueTime;
+
+create table MenuCategories (
+MenuCategoriesID SMALLINT primary key,
+MenuCategoriesName VARCHAR(20),
+MenuTimeID SMALLINT);
+
+create table MenuDetail (
+MenuID INT primary key,
+MenuName VARCHAR(20),
+MenuDescription VARCHAR(200),
+Price DECIMAL(10,2),
+MenuCategoriesID SMALLINT);
+
+create table Appetizer(
+Image varchar(200),
+ItemName VARCHAR(20),
+ShortDescription VARCHAR(200),
+Price DECIMAL(10,2));
+
+
+create table MainCourse(
+Image varchar(200),
+ItemName VARCHAR(20),
+ShortDescription VARCHAR(200),
+Price DECIMAL(10,2));
+
+
+create table Drinks(
+Image varchar(200),
+ItemName VARCHAR(20),
+ShortDescription VARCHAR(200),
+Price DECIMAL(10,2));
+
+
+create table Desserts(
+Image varchar(200),
+ItemName VARCHAR(20),
+ShortDescription VARCHAR(200),
+Price DECIMAL(10,2));
+
+insert into Appetizer values('Apple-Blue-Cheese','Apple-Blue-Cheese','Apple-Blue-Cheese',12.99);
