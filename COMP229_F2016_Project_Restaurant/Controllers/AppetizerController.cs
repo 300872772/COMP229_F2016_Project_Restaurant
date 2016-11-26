12644559274df278947fa26ae224b9427ac00093
Aangi <aashah.6198@gmail.com>
2016-11-25 19:14:28 -0500

    added controllers

---
+++ COMP229_F2016_Project_Restaurant/Controllers/AppetizerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP229_F2016_Project_Restaurant.Controllers
+{
+    public class AppetizerController : Controller
+    {
+        // GET: Appetizer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP229_F2016_Project_Restaurant/Controllers/DrinksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP229_F2016_Project_Restaurant.Controllers
+{
+    public class DrinksController : Controller
+    {
+        // GET: Drinks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
